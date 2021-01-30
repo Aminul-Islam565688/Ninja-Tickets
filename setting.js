3d149a6033052be2b877ec06bd66df434dbdced9
Aminul Islam <aminulislam565688@gmail.com>
2021-01-30 17:13:06 +0600

    just add

--- setting.js
+++ setting.js
@@ -1,14 +1,17 @@
 //to make Input Value Read Only
-document.getElementById("FirstClass-Cost").readOnly = true;
-document.getElementById("Economy-Cost").readOnly = true;
-document.getElementById("Economy-Center-Input").readOnly = true;
-document.getElementById("FirstClass-Center-Input").readOnly = true;
+readOnly("FirstClass-Cost");
+readOnly("Economy-Cost");
+readOnly("Economy-Center-Input");
+readOnly("FirstClass-Center-Input");
+
+function readOnly(Idname){
+    document.getElementById(Idname).readOnly = true;
+}
 
 
-//
+//Button Function for Increment and Decrement and Cost
 function button(ticketClass,buttonCase){
-    let flightClass = document.getElementById(ticketClass+"-Center-Input").value;
-    let flightClassNumber = parseInt(flightClass);
+    let flightClassNumber = stringToNumber(ticketClass)
     let upDownNumber = 0;
     if(buttonCase == true){
         upDownNumber = flightClassNumber +1;
@@ -16,17 +19,32 @@ function button(ticketClass,buttonCase){
     else if(buttonCase == false && flightClassNumber > 0){
         upDownNumber = flightClassNumber -1;
     }
-    
     let ticketCostTotal = 0;
 
     if(ticketClass == "FirstClass"){
-            ticketCostTotal = 150*upDownNumber;
+        ticketCostTotal = 150*upDownNumber;
     }
     else{
-            ticketCostTotal = 100*upDownNumber;
+        ticketCostTotal = 100*upDownNumber;
     }
-    document.getElementById(ticketClass+"-Cost").value=ticketCostTotal;
+    document.getElementById(ticketClass+"-Cost").value="$"+ticketCostTotal;
     document.getElementById(ticketClass+"-Center-Input").value=upDownNumber;
+    calculator();
+}
+
+//Calculator
+function calculator(){
+    let firstClassTickets = stringToNumber("FirstClass");
+    let economyClassTickets = stringToNumber("Economy");
+
+    let subTotal = firstClassTickets*150 + economyClassTickets*100;
+    document.getElementById("Sub-Total").innerText=subTotal;
+
+    let vat = subTotal*0.1;
+    document.getElementById("Vat").innerText=vat;
+
+    let total = subTotal + vat;
+    document.getElementById("Total").innerText=total;
 }
 
 
@@ -36,17 +54,25 @@ function stringToNumber(ticketClass){
     let stringToNumber = 0;
     if(ticketClass == "FirstClass" || ticketClass == "Economy"){
         stringValue = document.getElementById(ticketClass + "-Center-Input").value;
-        stringToNumber = parseInt(stringValue);
-        return stringToNumber;
     }
     else if(ticketClass == "FirstClass-Cost" || ticketClass == "Economy-Cost"){
         stringValue = document.getElementById(ticketClass).value;
-        stringToNumber = parseInt(stringValue);
-        return stringToNumber;
     }
     else if(ticketClass == "Sub-Total" || ticketClass == "Vat" || ticketClass == "Total"){
         stringValue = document.getElementById(ticketClass).innerText;
-        stringToNumber = parseInt(stringValue);
-        return stringToNumber;
     }
+    stringToNumber = parseInt(stringValue);
+    return stringToNumber;
+}
+
+
+//to Add Dollar Sign
+dollarSign("FirstClass");
+dollarSign("Economy");
+
+
+//Dollar Function
+function dollarSign(inputId){
+    let addDollar = stringToNumber(inputId+"-Cost")
+    document.getElementById(inputId+"-Cost").value="$"+addDollar;
 }
